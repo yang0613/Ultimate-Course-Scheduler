0b1fe4fdbd9771cc20d3f48caba28009fc11d572
Wenhao Yang <wyang39@ucsc.edu>
2022-04-19 10:17:44 -0700

    updated database entry data structure

--- backend/database/data.sql
+++ backend/database/data.sql
@@ -2,11 +2,11 @@
 DELETE FROM Classes;
 INSERT INTO Classes(classID, className, credit, diffculty, quarters, instructor) 
 VALUES (
-    'CSE 12', 'Computer Systems and Assembly Language and Lab', 7, 5, '{Fall,Winter,Spring,Summer}', 
-    '{The Staff, Tracy Larrabee, Darrell Long, Jose Renau Ardevol, Matthew Guthaus, Max Dunne, Sagnik Nath}'
+    'CSE 12', 'Computer Systems and Assembly Language and Lab', 7, 5, '["Fall","Winter","Spring","Summer"]', 
+    '["The Staff", "Tracy Larrabee", "Darrell Long", "Jose Renau Ardevol", "Matthew Guthaus", "Max Dunne", "Sagnik Nath"]'
 );
 DELETE FROM Requirements;
 INSERT INTO Requirements(classID, preReq, majorReq, gradReq, majorElective) 
 VALUES (
-    'CSE 12', '{CSE 5J,CSE 20,CSE 30,BME 160}', '{Computer Science B.S.}', '{Computer Science B.S.}', '{}'
+    'CSE 12', '["CSE 5J", "CSE 20","CSE 30","BME 160"]', '["Computer Science B.S."]', '["Computer Science B.S."]', '[]'
 );
