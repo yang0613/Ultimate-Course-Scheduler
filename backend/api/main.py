4303104be9b17f6aa711978acbee8643e4768cf5
Ziqi Fang <zfang1207@gmail.com>
2022-05-09 15:16:10 -0700

    working api with postgres sql

--- backend/api/main.py
+++ backend/api/main.py
@@ -13,7 +13,6 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
 while True:
 
     try:
@@ -47,72 +46,56 @@ def root(): #the function time doesn't matter; however it's better to have a des
 @app.get("/posts")
 def get_posts():
     #return{"data": "This is your posts"}
-    return{"data": my_posts}
+    cursor.execute("""SELECT * FROM public.posts""")
+    posts = cursor.fetchall()
+    #print(posts)
+    #return{"data": my_posts}
+    return{"data": posts}
 #every path parameter passed in as a string. Make sure you manually convert it into type you want
 @app.get("/posts/{id}")
 def get_post(id:int, response: Response):
-    #post = find_post(int(id))
-    post = find_post(id)
+    #post = find_post(id)
+    cursor.execute("""SELECT * FROM public.posts WHERE id = %s""", (str(id)))
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-        #response.status_code = status.HTTP_404_NOT_FOUND
-        #return {"message":f"post with id: {id} was not found"}
     return {"post_detail":post}
 # Post request
-'''
-@app.post("/post")
-def create_posts():
-    return {"message" : "Your post created successfully"}
-'''
-'''
-@app.post("/post")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return { "Your post created successfully": f"title: {payload['title']}, content: {payload['content']}"}
-'''
-'''
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return{"new_post" : f"title {payload['title']} content:{payload['content']}"}
-'''
 
 #schema of posts: title str, content str
 #load data into certain variable by referencing to a created schema class, which is 'Post' here
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    #print(post)
-    #print(post.dict())
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0,10000)
-    post_dict['id_num'] = randrange(0,10000)
-    my_posts.append(post_dict)
-    return{"data": post_dict}
-
-    #return{"message": "successfully created the post"}
+    #post_dict = post.dict()
+    #post_dict['id'] = randrange(0,10000)
+    #post_dict['id_num'] = randrange(0,10000)
+    #my_posts.append(post_dict)
+    #return{"data": post_dict}
+    cursor.execute("""INSERT INTO public.posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content,post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return{"data": new_post}
 
 #Delete a post
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int):
-    #find the index in the array that has required ID
-    # my_posts.pop(index)
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""DELETE FROM public.posts WHERE id = %s RETURNING *""", (str(id)))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
-    my_posts.pop(index)
     #return {"message": "post was successfully deleted"}
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 def update_post(id:int,post:Post):
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""UPDATE public.posts SET title = %s, content = %s, published = %s  WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
     #print(post)
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+    return {"data": updated_post}
     #return {'message': "updated post"}
 
