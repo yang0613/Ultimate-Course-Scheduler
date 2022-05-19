e6a7d625923a69ce0101748a445c173fb1d252a6
Shivika Sivakumar <shivikasiva@gmail.com>
2022-05-19 12:41:15 -0700

    stash

--- frontend/src/Script.js
+++ frontend/src/Script.js
@@ -2,7 +2,7 @@ export const post = (data) => {
     const response = fetch('http://127.0.0.1:8000/searchclass', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/string'
+            'Content-Type': 'application/json'
         },
         body: "data", //use JSON.stringify if and when needed, but currently string
     })
