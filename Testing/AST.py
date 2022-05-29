f0c045f2cfc43ef7df432db68db0da1a4fad972f
schan60 <schan60@ucsc.edu>
2022-05-29 14:49:39 -0700

    Copied parser component testing related files to /Testing/ for project review

---
+++ Testing/AST.py
@@ -0,0 +1,238 @@
+#!/usr/bin/python3.9
+from boolean import BooleanAlgebra
+from boolean import TOKEN_NOT
+from boolean import TOKEN_AND
+from boolean import TOKEN_OR
+from boolean import TOKEN_TRUE
+from boolean import TOKEN_FALSE
+from boolean import TOKEN_LPAR
+from boolean import TOKEN_RPAR
+from boolean import Expression
+from boolean import Symbol
+from boolean import Symbol
+from enumerate import int2word
+import parse
+
+
+TOKENS = { #Standard tokens from Boolean Algebra
+            '*': TOKEN_AND, '&': TOKEN_AND, 'and': TOKEN_AND,
+            '+': TOKEN_OR, '|': TOKEN_OR, 'or': TOKEN_OR,
+            '~': TOKEN_NOT, '!': TOKEN_NOT, 'not': TOKEN_NOT,
+            '(': TOKEN_LPAR, ')': TOKEN_RPAR,
+            '[': TOKEN_LPAR, ']': TOKEN_RPAR,
+            'true': TOKEN_TRUE, '1': TOKEN_TRUE,
+            'false': TOKEN_FALSE, '0': TOKEN_FALSE, 'none': TOKEN_FALSE
+        }
+
+
+
+class PrereqAlgebra(BooleanAlgebra):
+    """An AST implementation for a specific prerequisite.
+    """
+
+    def __init__(self):
+        super().__init__(allowed_in_token=',')
+
+
+    def tokenize(self, expr):
+
+        self.position = 0
+        self.previous_token = None, '', 0
+
+        def update_token(TOKEN, str, pos):
+            """Saves the latest token in history and update our 
+            positioning in the tokens
+            """
+            self.previous_token = TOKEN
+            if pos == self.position:
+                self.position = self.position + 1
+            return TOKEN, str, pos
+        
+
+        requirements = parse.requirements_list(expr)
+
+        for req in requirements:
+            if parse.requirement_found(req):
+                yield update_token(TOKEN_LPAR, '(', self.position)
+                for tok, args, expr, pos in parse.parse(req):
+                    # An expression that follows ( and / ( or is invalid
+                    req_obj = TOKENS_MATCH_SYMBOLS[tok](**args)
+                    if req_obj is not None:
+                        if self.previous_token == TOKEN_LPAR:
+                            if req_obj == TOKEN_OR:
+                                yield update_token(TOKEN_FALSE, 'false', self.position)
+                            elif req_obj == TOKEN_AND:
+                                yield update_token(TOKEN_TRUE, 'true', self.position)
+                        yield update_token(req_obj, expr, self.position+pos)
+                ##Check to see for unbalanced operators from our regex
+                ##expression. When every possible requirement is encountered
+                ##for, no unbalanced operators will occur and these yields 
+                ##will never run
+                if self.previous_token == TOKEN_OR:
+                    yield update_token(TOKEN_FALSE, 'false', self.position)
+                elif self.previous_token == TOKEN_AND:
+                    yield update_token(TOKEN_TRUE, 'true', self.position)
+                
+                yield update_token(TOKEN_RPAR, ')', self.position)
+                yield update_token(TOKEN_AND, 'and', self.position)
+        #Required since "ANDs" require dual operators
+        yield update_token(TOKEN_TRUE, 'true', self.position)
+
+algebra = PrereqAlgebra()
+
+class Course(Symbol):
+    """A variable representing the following format: 
+    className classID[optLetter]
+    """
+    def __init__(self, expr, **kwargs):
+        self.expr = expr
+        super().__init__(expr)
+    
+    def _subs(self, substitutions, default, simplify):
+        if self.expr in substitutions.keys():
+            return substitutions[self.expr]
+
+class ConcurrentEnrollment(Symbol):
+    """A class representing a Concurrent Enrollment requirement in our
+    AST
+    """
+    def __init__(self, expr, concurrent_list, prevEnrollAllowed):
+        self.expr = expr
+        self.concurrent_list = concurrent_list
+        self.prevEnrollAllowed = prevEnrollAllowed
+        super().__init__(expr)
+
+    def _subs(self, substitutions, default, simplify):
+        for course in self.concurrent_list:
+            if self.prevEnrollAllowed:
+                all_classes = substitutions | self.quarter
+                test_course = algebra.parse(course).subs(all_classes).simplify()
+            else:
+                test_course = algebra.parse(course).subs(self.quarter).simplify()
+            if test_course == algebra.TRUE:
+                return algebra.TRUE
+        return self
+    
+    def setQuarterClasses(self, classes):
+        """A helper function for constraint.py to check for concurrent
+        enrollment
+        """
+        subs = {}
+        for course in classes:
+            subs[course] = algebra.TRUE
+        self.quarter = subs
+
+class NumFrom(Symbol):
+    """A class representing any Num_From expression
+    """
+    def __init__(self, expr, name, list, total, discounts=[], quarter=None, **kwargs):
+        self.course_list = list
+        self.discounts = discounts
+        self.total = total
+        self.quarter = quarter
+        self.name = name
+        self.expr = expr
+        if self.name:
+            #Temporarily remove the list from the expression
+            #because the expression's list can be very long and
+            #cloud up the results
+            self.expr = self.expr_without_list()
+        super().__init__(expr)
+
+    def expr_without_list(self):
+        """Returns the expression without the list
+        From "one from: "NumFrom name" COURSE 1A, COURSE1B, or COURSE 3A
+        to "one from: "NumFrom name"
+        """
+        return f"{int2word[self.total]} from: \"{self.name}\""
+
+    def count(self, substitutions):
+        count = 0
+        for course in self.course_list:
+            test_course = algebra.parse(course).subs(substitutions).simplify()
+            if test_course == algebra.TRUE:
+                count = count + 1
+        for discount in self.discounts:
+            count = count + min(discount.total, discount.count(substitutions))
+        return count
+
+    def _subs(self, substitutions, default, simplify):
+        remaining_class = self.total - self.count(substitutions)
+        if remaining_class <= 0:
+            return algebra.TRUE
+        else:
+            new_expr = self.expr.replace(int2word[self.total],
+            int2word[remaining_class], 1)
+            return self.__class__(expr=new_expr,
+                                  name=self.name,
+                                  list=self.course_list,
+                                  total = remaining_class,
+                                  discounts = self.discounts)
+
+def paranthesis_symbol(expr='', paranthesis=''):
+    if paranthesis in ('(', ')'):
+        return TOKENS[paranthesis]
+    return None
+
+TOKENS_MATCH_SYMBOLS = {
+    parse.TOKEN_COURSE: Course,
+    parse.TOKEN_CONCURRENT: ConcurrentEnrollment,
+    parse.TOKEN_NUM_FROM: NumFrom,
+    parse.TOKEN_OP: lambda op: TOKENS[op.lower()],
+    parse.TOKEN_PAR: paranthesis_symbol
+}
+
+
+def split_ast(parse: Expression):
+    """Given an Abstract Syntax Tree parse object,
+    split the tree at depth 1 into a list 
+
+    Args:
+        parse (Expression): An parse object
+
+    Returns:
+        str: A list representation of our AST
+    """
+    if parse.isliteral:
+        return([str(parse)])
+    replace_tokens = [("&", " and "), ("|", " or ")]
+    arg_list = []
+    for arg in parse.args:
+        arg = str(arg).strip()
+        for tok, replacement in replace_tokens:
+            arg = arg.replace(tok, replacement)
+        arg_list.append(arg)
+    return arg_list
+
+def missing_requirements(parse: Expression):
+    """Given an Abstract Syntax Tree parse reprsenting our
+    prerequisites, convert this into a string fits more to the format of
+    UCSC's prerequisites. 
+
+    Args:
+        parse (Expression): An parse object from PrereqAlgebra()
+
+    Returns:
+        str: A string of prerequisites
+    """
+    missing_reqs = split_ast(parse)
+    if parse.pretty().startswith("OR"):
+        return "; or ".join(missing_reqs)
+    return missing_reqs
+
+
+def convert_schedule(schedule):
+    """Convert's a JSON schedule into an subsitution dictionary mapping
+    following boolean.py specifications
+
+    Args:
+        schedule (JSON): JSON Object holding our Schedule
+
+    Returns:
+        A dictionary of symbols with an substituted value TRUE
+    """
+    subs = {}
+    for year, quarter, classes in parse.schedule_tokens(schedule):
+        for course in classes:
+            subs[course] = algebra.TRUE
+    return subs
---
+++ Testing/parse.py
@@ -0,0 +1,364 @@
+#!/usr/bin/python3.9
+TOKEN_DISCOUNTS = 'discounts'
+TOKEN_NAME = 'name'
+TOKEN_LIST = 'list'
+TOKEN_DUE_DATE = 'quarter'
+TOKEN_COURSE = 'course_list'
+TOKEN_OP = 'op'
+TOKEN_COUNT = 'total'
+TOKEN_TYPE = 'type'
+TOKEN_CONCURRENT = 'concurrent'
+TOKEN_CONCURRENT_LIST = 'concurrent_list'
+TOKEN_PREV_CONCURRENT = 'prevEnrollAllowed'
+TOKEN_MULTIPLE_CONCURRENT = 'multiple'
+TOKEN_NUM_FROM = 'num_from'
+TOKEN_NUM_FROM_EACH = 'each_list'
+
+TOKEN_REQUIREMENT = 'req'
+TOKEN_POS = 'position'
+TOKEN_EXPR = 'expr'
+TOKEN_RECOMMEND = 'rec'
+TOKEN_NO_CREDIT = 'no_credit'
+TOKEN_BLACKLIST = 'blacklist'
+TOKEN_PAR = 'paranthesis'
+TOKEN_LR_PAR ='left_or_right_paranthesis'
+
+import AST
+from re import compile
+from enumerate import word2int
+"""
+Responsible for parsing a prerequisite string into tokens from the 
+following patterns
+
+TOKEN_COUNT from: TOKEN_LIST [discounting TOKEN_COUNT from: TOKEN_LIST]* [by
+quarter TOKEN_DUE_DATE]
+"one from: ANTH 2, SOCY 1, SOCY 10, SOCY 15, PHIL 21, PHIL 22, PHIL 24,
+PHIL 28, or PHIL 80G"
+
+or
+
+TOKEN_COURSE [TOKEN_OP TOKEN_COURSE]*
+"STAT 7 and STAT 7L, or ECON 113 or OCEA 90"
+
+or
+
+[Previous or] Concurrent enrollment in TOKEN_LIST [is required]
+
+where portions covered by [] are option and * indicates that it may 
+repeat multiple times
+
+"""
+
+#These tokens are identifiers 
+
+#Regex pattern matches responsible for parsing our prerequisites
+op = f"(?P<{TOKEN_OP}>(and|or))"
+course = f"(?P<{TOKEN_COURSE}>[A-Z]+ *[\d]+[A-Z]?)"
+courses_list = compile(f" *(Completion *of *)?{course},? *{op}? *")
+nums = f"(?P<{TOKEN_COUNT}>one|two|three|four|five|six|seven|eight|nine|ten)"
+req_list_better = f"(?P<{TOKEN_LIST}>(.+?))"
+#Modified regex expression from
+#https://stackoverflow.com/questions/33400570/regex-to-parse-a-comma-separated-list-excluding-content-within-parentheses
+seperate_req_list = compile(
+    f" *(?P<{TOKEN_LIST}>[^,(]*(?:\([^)]*\))*[^,]*)(, *or)? *")
+types = f"(?P<{TOKEN_TYPE}>(?:start *discounting|discounting|by *quarter\
+ *(?P<{TOKEN_DUE_DATE}>\d+)|$))"
+num_from = compile(
+    f"( *(?P<{TOKEN_NUM_FROM_EACH}>{nums} *from: *(\"(?P<{TOKEN_NAME}>.*?)\")? *{req_list_better} *{types}) *)"
+)
+delim = compile(r"(; *and *|\. *|; *(?! *or)|\n)")
+concurrent_enrollment_list = f"(?P<{TOKEN_CONCURRENT_LIST}>([A-Z]+ *[\d]+[A-Z]?)\
+(,? *(and|or)? *[A-Z]+ *[\d]+[A-Z]?)*)"
+concurrent_enrollment = compile(
+f"(?P<{TOKEN_CONCURRENT}>(?P<{TOKEN_PREV_CONCURRENT}>[pP]revious *or)? *([cC]oncurrent *enrollment *in|[cC]orequisite:) *\
+{concurrent_enrollment_list} *(is *required)? *)"
+)
+missing_req_list = compile(r"&\s*(?![^()]*\))")
+#Special paranthesis pattern to avoid phrases like (MPE)
+paranthesis = compile(f"(?P<{TOKEN_PAR}>\([a-zA-Z]+\)|(?P<{TOKEN_LR_PAR}>\(|\)))")
+
+def split(pattern, expr: str):
+    """Split a string by an regex expression, and remove any empty
+    strings and existing elements that still match our expression
+    """
+    list = pattern.split(expr)
+    for elem in list[:]:
+        if elem == '' or pattern.match(elem):
+            list.remove(elem)
+    return list
+def requirements_list(expr: str):
+    """Given an string of prerequisites, return a structured list
+    of strings that represent the prerequisite.
+
+    Args:
+        expr (list): A list of requirements 
+    """
+    return split(delim, expr)
+
+def course_tokens(course_list: str):
+    """Returns a stream of courses by their names and starting character
+    positions
+
+    Args:
+        course_list (str): A string representing a list of courses
+        in the following format:
+        
+        course (and|or) course[,] (and|or) course[,] ... course
+
+    Yields:
+        (Iterable): A stream of course tokens
+    """
+    for match in courses_list.finditer(course_list):
+        yield {TOKEN_COURSE: match[TOKEN_COURSE]}
+
+
+def num_from_tokens(num_from_expr: str):
+    """Returns a stream of dictionary tokens for 
+    some arbitrary number of classes that is required from some list.
+
+    Args:
+        num_from_expr (str): A string representing a num from expression
+        from the following format:
+
+        "num from: Course, Course, Course, or ANY LIST [start 
+        discounting num2 from: Course, Course, Course, or ANY LIST] 
+        [discounting num3 from: ...]... [by quarter date]
+
+
+    Yields:
+        (Iterable): A stream of requirement list tokens
+    """
+    for match in num_from.finditer(num_from_expr):
+        toks = {}
+        components = [TOKEN_COUNT, TOKEN_NAME, TOKEN_DUE_DATE, TOKEN_TYPE]
+        for comp in components:
+            valid_result = match[comp]
+            if valid_result:
+                toks[comp] = match[comp]
+            else:
+                toks[comp] = None
+
+        count = toks[TOKEN_COUNT]
+        due_date = toks[TOKEN_DUE_DATE]
+        #Convert the token "one" in "one from:" to an integer value
+        toks[TOKEN_COUNT] = word2int[count]
+        if due_date:
+            #Convert the token "6" in "by quarter 6" to an integer value
+            toks[TOKEN_DUE_DATE] = int(due_date)
+        toks[TOKEN_LIST] = list(reqs_list_tokens(match[TOKEN_LIST]))
+        toks[TOKEN_EXPR] = match[TOKEN_NUM_FROM_EACH]
+        yield toks
+
+def build_num_from(num_from_expr: str):
+    """Due to the num_from expresison being more complex, there exists"""
+    num_from_args = {}
+    num_from_args[TOKEN_DISCOUNTS] = discounts = []
+
+    each_num_from = num_from_tokens(num_from_expr)
+
+    first_num_from = next(each_num_from)
+
+    num_from_args.update(first_num_from)
+
+    for tok in each_num_from:
+        due_date = tok[TOKEN_DUE_DATE]
+        discounts.append(AST.NumFrom(**tok))
+        if due_date:
+            num_from_args[TOKEN_DUE_DATE] = due_date
+    yield num_from_args
+
+
+def reqs_list_tokens(reqs_list: str):
+    """Returns a stream of courses that is the list of a "num_from" 
+    expression
+
+    Args:
+        reqs_list (str): A list of courses following what comes after
+        "num from:" 
+
+        Example:
+
+        An "num_from" expression 
+
+        'three from: CSE 101, ECON 104, (MATH 19A and MATH 19B), or
+        PHYS 23 by quarter 6'
+
+        would require the inputs of
+
+        'CSE 101, ECON 104, (MATH 19A and MATH 19B), or PHYS 23'
+
+        for this function.
+    Yields:
+        (Iterable): A stream of courses, either as a course name
+        or an boolean expression of courses
+    """
+    for match in seperate_req_list.finditer(reqs_list):
+        course_list = match[TOKEN_LIST]
+        if course_list:
+            yield course_list
+            
+def concurrent_enrollment_tokens(conc_enr: str):
+    """Returns a stirng of classes that requires concurrent/previous enrollment
+    from an concurrent enrollment expression
+    
+
+    Args:
+        conc_enr (str): A set of classes following the format:
+        
+        "[Previous or] [cC]oncurent enrollment in COURSE_LIST is required. and/or..."
+    Yields:
+        (Iterable): A boolean expression of a stream of courses that 
+        requires either a single or multiple concurrent enrollments 
+    """
+    for match in concurrent_enrollment.finditer(conc_enr):
+        toks = {}
+        allowPrevious = match[TOKEN_PREV_CONCURRENT]
+        if allowPrevious:
+            toks[TOKEN_PREV_CONCURRENT] = True
+        else:
+            toks[TOKEN_PREV_CONCURRENT] = False
+        course_list = match[TOKEN_CONCURRENT_LIST]
+        toks[TOKEN_CONCURRENT_LIST] = [course for course in reqs_list_tokens(course_list)]
+        yield toks
+
+def token(TOKEN_ID, kwargs, expr, pos):
+    """Abstraction method that specifies what format a token should be
+    returned as
+
+    Args:
+        TOKEN_ID (str): A token identifier matching the the associated token object
+        kwargs (**kwargs): Standard keyword arguments for the token object
+        expr (str): The token expression
+        pos (int): An integer representing the initial position of the expression
+    """
+    return TOKEN_ID, kwargs, expr, pos
+
+def dummy_token(expr: str):
+    """A dummy token that yields nothing because it does not need
+    any additional arguments
+
+    Args:
+        expr (str): Any string input
+
+    Yields:
+        dict: An empty dictionary
+    """
+    return {}
+
+def paranthesis_token(par: str):
+    """
+    Yields either L_PAR '(' or R_PAR ')' as long as it isn't the
+    surrounding paranthesis encompass only a word. In other words,
+    strings like (WORD) won't yield '(' or ')' in that case.
+
+    Args:
+        par (str): Any string containing paranthesis
+
+    Yields:
+        A dummy token
+    """
+    for match in paranthesis.finditer(par):
+        legal_par = match[TOKEN_LR_PAR]
+        if legal_par:
+            yield {TOKEN_PAR: legal_par}
+        else:
+            yield dummy_token(par)
+
+
+TOKENS_MATCH_FUNCTIONS = {
+    TOKEN_COURSE: course_tokens,
+    TOKEN_CONCURRENT: concurrent_enrollment_tokens,
+    TOKEN_NUM_FROM: build_num_from,
+    TOKEN_PAR: paranthesis_token
+}
+
+TOKENS_MATCH_PATTERNS = {
+    TOKEN_NUM_FROM: f'(?P<{TOKEN_NUM_FROM}>'+ num_from.pattern +'+)',
+    TOKEN_CONCURRENT: concurrent_enrollment.pattern,
+    TOKEN_COURSE: course,
+    TOKEN_PAR: paranthesis.pattern
+}
+
+"""
+Somethings our regex pattern finds a match that partially looks like
+a requirement that is in fact completely irrelevant/optional. We implement
+a list of blacklisted patterns that we specifically instruct our program
+not to consider them as a requirement.
+"""
+BLACKLIST_PATTERNS = {
+    TOKEN_RECOMMEND: r' *recommended *as *prerequisite *to *this *course *',
+    TOKEN_NO_CREDIT: r" *may *not *enroll * in *"
+}
+
+
+extract_requriements = compile(
+f' *(,? *{op})? *' +
+f'(?P<{TOKEN_REQUIREMENT}>' + '|'.join(TOKENS_MATCH_PATTERNS.values()) + ')'
+)
+
+blacklist_requirements = compile(
+f'(?P<{TOKEN_BLACKLIST}>' + '|'.join(BLACKLIST_PATTERNS.values()) + ')')
+
+def find_req(match):
+    """Given an match, find exactly the tokens of what requirements the match
+    exactly corresponds to"""
+    for tok, func in TOKENS_MATCH_FUNCTIONS.items():
+        req, req_pos = match[tok], match.start(tok)
+        if req:
+            args = next(func(req))
+            args[TOKEN_EXPR] = req
+            return token(TOKEN_ID=tok, kwargs=args, 
+                        pos=req_pos, expr=req)
+
+def parse(expr: str):
+    """The main tokenizer function that extracts requirements into the
+    neccesary bits and pieces to create our Abstract Syntrax Tree
+    """
+    for match in extract_requriements.finditer(expr):
+        op = match[TOKEN_OP]
+        pos = match.start(TOKEN_OP)
+        if op:
+            yield token(TOKEN_ID=TOKEN_OP, kwargs={TOKEN_OP: op}, pos=pos, expr=op)
+        yield find_req(match)
+
+def requirement_found(expr: str):
+    """If an requirement is found within our expression, return true.
+    Otherwise false.
+
+    Args:
+        expr (str): A requirement expression
+
+    Returns:
+        boolean: A status indicator for whether a requirement was found
+    """
+    return extract_requriements.search(expr) is not None and blacklist_requirements.search(expr) is None
+
+
+def schedule_tokens(schedule):
+    """Returns a stream of classes identified by year and quarter
+    in the schedule.
+
+    Args:
+        schedule (JSON): JSON Object holding our Schedule
+
+    Returns:
+        A stream of classes with their year and quarter
+    """
+    for year in schedule.keys():
+        for quarter, classes in schedule[year].items():
+                yield year, quarter, classes
+
+def classIDS(schedule):
+    """Converts a schedule into a tuple of classes for the use of
+    query.py/allClassByID
+
+    Args:
+        schedule (JSON): JSON Object holding our Schedule
+
+    Returns:
+        A tuple of classes
+    """
+    classIDs = []
+    for _, _, classes in schedule_tokens(schedule):
+        classIDs.extend(classes)
+    return tuple(classIDs)
