b673ee3e38c99a63394d4cffc7e54cf79be06ed6
Mhia Mojica <msmojica@ucsc.edu>
2022-05-29 13:43:30 -0700

    changed the logo

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -41,7 +41,7 @@ class LandingPage extends React.Component
     return (
       <div class="container has-text-centered">
         <figure class="image is-inline-block ">
-          <img src="slugLogo.png" alt="Placeholder image"></img>
+          <img src="slugLogoHorizontal.png" alt="Placeholder image"></img>
         </figure>
         <br></br>
 
